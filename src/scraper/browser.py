d93384bc03990e0573f252ece721b86c71332e38
natan <natanael0junior@gmail.com>
2026-03-12 13:31:06 -0300

    feat: integrate with captcha api and polish code

--- src/scraper/browser.py
+++ src/scraper/browser.py
@@ -1,22 +1,33 @@
 from playwright.async_api import async_playwright
 from playwright_stealth import Stealth
+import asyncio
 
 
 class BrowserManager:
     def __init__(self):
+        self.semaphore = asyncio.Semaphore(5)
+        self.detailsLock = asyncio.Lock()
         self.stealth = None
         self.playwright = None
         self.context = None
 
-    async def start(self):
+    async def start(self, headless=True):
         self.stealth = Stealth()
         self.playwright_cm = self.stealth.use_async(async_playwright())
         self.playwright = await self.playwright_cm.__aenter__()
 
         self.context = await self.playwright.chromium.launch_persistent_context(
             "./user_data",
-            headless=False,
+            headless=headless,
             viewport={"width": 1920, "height": 1080},
+            args=[
+                "--disable-blink-features=AutomationControlled",
+                "--start-maximized",
+                "--no-sandbox",
+                "--disable-setuid-sandbox",
+                "--disable-dev-shm-usage",  # Evita crashes em containers/Linux
+            ],
+            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
         )
 
     async def newPage(self):
@@ -25,3 +36,14 @@ class BrowserManager:
     async def close(self):
         await self.context.close()
         await self.playwright.stop()
+
+
+async def main():
+    browser = BrowserManager()
+    await browser.start(headless=False)
+    await browser.newPage()
+    await asyncio.sleep(5000)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -1,6 +1,7 @@
 import asyncio
 import base64
-from browser import BrowserManager
+from src.scraper.browser import BrowserManager
+from src.services.telegram_captcha import TelegramCaptchaManager
 
 
 async def scrollAndClick(element) -> None:
@@ -12,7 +13,8 @@ async def scrollAndClick(element) -> None:
 
 async def search(page, searchString: str) -> None:
     # Acha e clica no botao pessoa fisica e juridica (clica na div pai pois o botao ta hidden)
-    button = page.locator("#btnPessoa").locator("xpath=..")
+    await page.wait_for_load_state("domcontentloaded")
+    button = page.locator("#btnPessoa").locator("..")
     await scrollAndClick(button)
 
     # Acha e clica no botao Busca de pessoa fisica
@@ -20,14 +22,14 @@ async def search(page, searchString: str) -> None:
     await scrollAndClick(button)
 
     # Filtra a busca por beneficiário de programa social
-    filterAcc = page.locator("text=REFINE A BUSCA").locator("xpath=..")
-    chevDown = filterAcc.locator("xpath=..")
+    filterAcc = page.locator("text=REFINE A BUSCA").locator("..")
+    chevDown = filterAcc.locator("..")
     isActive = await chevDown.get_attribute("active")
     while isActive is None:
         await scrollAndClick(filterAcc)
         isActive = await chevDown.get_attribute("active")
 
-    checkBox = page.locator("#beneficiarioProgramaSocial").locator("xpath=..")
+    checkBox = page.locator("#beneficiarioProgramaSocial").locator("..")
     await scrollAndClick(checkBox)
 
     # Realiza a busca
@@ -37,9 +39,76 @@ async def search(page, searchString: str) -> None:
     return
 
 
-async def getBenefitTable(benefit_item, page) -> list:
-    goToDetailsBtn = benefit_item.locator("#btnDetalharBpc")
-    await scrollAndClick(goToDetailsBtn)
+async def backToBenefitPage(page):
+    # Volta pra página anterior e reabre a lista de beneficios
+    await page.go_back()
+    await page.wait_for_load_state("domcontentloaded")
+    filterAcc = page.locator("text=RECEBIMENTOS DE RECURSOS").locator("..")
+    chevDown = filterAcc.locator("..")
+    isActive = await chevDown.get_attribute("active")
+    while isActive is None:
+        await scrollAndClick(filterAcc)
+        isActive = await chevDown.get_attribute("active")
+
+
+async def getBenefitTable(benefit_item, page, captcha_manager, manager) -> list:
+    # Inicializa a resposta do possível captcha como nula
+
+    captcha_response = None
+    # Lock aqui para que cada agente só clique no botão de detalhar isoladamente, para evitar multiplos captchas
+    async with manager.detailsLock:
+        goToDetailsBtn = benefit_item.locator("#btnDetalharBpc")
+        await scrollAndClick(goToDetailsBtn)
+
+        title = await page.title()
+
+        if title == "Human Verification":
+            begin_button = page.locator(".amzn-captcha-verify-button")
+            await scrollAndClick(begin_button)
+
+            await asyncio.sleep(0.5)
+
+            captcha_div = page.locator("#root").first
+            captcha_screenshot = await captcha_div.screenshot()
+
+            # Timeout no evento e tratar caso não haja resposta a tempo
+            try:
+                captcha_response = await asyncio.wait_for(
+                    captcha_manager.send_captcha_and_wait(captcha_screenshot),
+                    timeout=30,
+                )
+            except asyncio.TimeoutError:
+                await backToBenefitPage(page)
+                return ["Captcha demorou demais para resolver."]
+            except Exception as e:
+                await backToBenefitPage(page)
+                return [f"Algum erro ocorreu durante a resolução do captcha!\n{e}."]
+
+            canvas = page.locator("canvas").first
+            if not captcha_response:
+                return ["O Captcha não foi respondido"]
+
+            for char in captcha_response:
+                if char.isdigit() and 1 <= int(char) <= 9:
+                    index = int(char) - 1
+                    col = index % 3
+                    row = index // 3
+
+                    x = (col + 0.5) * (320 / 3)
+                    y = (row + 0.5) * (320 / 3)
+
+                    await canvas.click(position={"x": x, "y": y})
+                    await asyncio.sleep(0.4)
+
+            verify_button = page.locator("#amzn-btn-verify-internal")
+            await verify_button.click()
+            await page.wait_for_load_state("networkidle")
+
+    # Testar se o captcha foi resolvido aqui
+    title = await page.title()
+    if title == "Human Verification":
+        await backToBenefitPage(page)
+        return ["O Captcha não foi resolvido a tempo"]
 
     # Pega as colunas do beneficio
     head = page.locator("thead").locator("tr").first
@@ -71,43 +140,36 @@ async def getBenefitTable(benefit_item, page) -> list:
         benefit_table.append(row_data)
 
     # Volta pra página anterior e reabre a lista de beneficios
-    await page.go_back()
-    filterAcc = page.locator("text=RECEBIMENTOS DE RECURSOS").locator("xpath=..")
-    chevDown = filterAcc.locator("xpath=..")
-    isActive = await chevDown.get_attribute("active")
-    while isActive is None:
-        await scrollAndClick(filterAcc)
-        isActive = await chevDown.get_attribute("active")
-
+    await backToBenefitPage(page)
     return benefit_table
 
 
-async def parsePage(page) -> dict:
+async def parsePage(page, captcha_manager, browserManager) -> dict:
     # Abre a aba de recebimento
-    filterAcc = page.locator("text=RECEBIMENTOS DE RECURSOS").locator("xpath=..")
-    chevDown = filterAcc.locator("xpath=..")
+    filterAcc = page.locator("text=RECEBIMENTOS DE RECURSOS").locator("..")
+    chevDown = filterAcc.locator("..")
     isActive = await chevDown.get_attribute("active")
     while isActive is None:
         await scrollAndClick(filterAcc)
         isActive = await chevDown.get_attribute("active")
 
     # Tira o print da janela
-    screenshot_bytes = await page.screenshot()
+    screenshot_bytes = await page.screenshot(path="new_screenshot.png")
     screenshot_b64 = base64.b64encode(screenshot_bytes).decode("utf-8")
 
     # Parse data
     # Nome
-    divName = page.locator("strong:has-text('Nome')").locator("xpath=..").first
+    divName = page.locator("strong:has-text('Nome')").locator("..").first
     spanName = divName.locator("span")
     textName = await spanName.inner_text()
 
     # CPF
-    divCPF = page.locator("strong:has-text('CPF')").locator("xpath=..").first
+    divCPF = page.locator("strong:has-text('CPF')").locator("..").first
     spanCPF = divCPF.locator("span")
     textCPF = await spanCPF.inner_text()
 
     # Local
-    divLocal = page.locator("strong:has-text('Localidade')").locator("xpath=..").first
+    divLocal = page.locator("strong:has-text('Localidade')").locator("..").first
     spanLocal = divLocal.locator("span")
     textLocal = await spanLocal.inner_text()
 
@@ -124,7 +186,9 @@ async def parsePage(page) -> dict:
         amountText = await amount.inner_text()
         parsedAmount = amountText.replace(".", "").replace(",", ".")
 
-        bf_table = await getBenefitTable(table_item, page)
+        bf_table = await getBenefitTable(
+            table_item, page, captcha_manager, browserManager
+        )
 
         benefits.append(
             {"name": auxType, "totalAmount": parsedAmount, "details": bf_table}
@@ -141,61 +205,65 @@ async def parsePage(page) -> dict:
     return data
 
 
-async def scrape(browser, searchString: str) -> dict:
-    # Abrindo o browser
+async def scrape(browser, captcha_manager, searchString: str) -> dict:
+    async with browser.semaphore:
+        # Cria a página
+        page = await browser.newPage()
 
-    page = await browser.newPage()
+        # Poderia ir direto para https://portaldatransparencia.gov.br/pessoa-fisica/busca/lista porem especificação nao deixa claro
+        await page.goto("https://portaldatransparencia.gov.br/")
 
-    # Poderia ir direto para https://portaldatransparencia.gov.br/pessoa-fisica/busca/lista porem especificação nao deixa claro
-    await page.goto("https://portaldatransparencia.gov.br/")
+        # Caminha na pagina e realiza a busca
+        await search(page, searchString)
 
-    # Caminha na pagina e realiza a busca
-    await search(page, searchString)
-
-    # Extrai o número de resultados encontrados
-    countResults = page.locator("#countResultados")
-    try:
-        await countResults.wait_for(state="visible", timeout=15000)
-    except TimeoutError:
-        # Timeout
-        print("Timeout: Results count did not appear.")
-        return {
-            "sucess": False,
-            "error": "Response time exceeded. Website may be unstable.",
-        }
-    except Exception as e:
-        # Qualquer outro error
-        print(f"Unexpected error: {e}")
-        return {"sucess": False, "error": "Unexpected error."}
-
-    numberOfResults = await countResults.inner_text()
-
-    # Pega o primeiro resultado se ele existe
-    if numberOfResults == "0":
-        # Retornar o json formatado para deixar a mensagem correta.
-        if searchString.isnumeric():
-            data = {
-                "error": "Não foi possível retornar os dados no tempo de resposta solicitado"
+        # Extrai o número de resultados encontrados
+        countResults = page.locator("#countResultados")
+        try:
+            await countResults.wait_for(state="visible", timeout=15000)
+        except TimeoutError:
+            # Timeout
+            print("Timeout: Results count did not appear.")
+            await page.close()
+            return {
+                "sucess": False,
+                "error": "Response time exceeded. Website may be unstable.",
             }
-        else:
-            data = {
-                "error": f"Foram encontrados 0 resultados para o termo {searchString}"
-            }
+        except Exception as e:
+            # Qualquer outro error
+            await page.close()
+            print(f"Unexpected error: {e}")
+            return {"sucess": False, "error": "Unexpected error."}
+
+        numberOfResults = await countResults.inner_text()
+
+        # Pega o primeiro resultado se ele existe
+        if numberOfResults == "0":
+            # Retornar o json formatado para deixar a mensagem correta.
+            if searchString.isnumeric():
+                data = {
+                    "error": "Não foi possível retornar os dados no tempo de resposta solicitado"
+                }
+            else:
+                data = {
+                    "error": f"Foram encontrados 0 resultados para o termo {searchString}"
+                }
+            await page.close()
+            return data
+
+        result = page.locator(".link-busca-nome").first
+        await scrollAndClick(result)
+
+        data = await parsePage(page, captcha_manager, browser)
+        await page.close()
         return data
-
-    result = page.locator(".link-busca-nome").first
-    await scrollAndClick(result)
-
-    data = await parsePage(page)
-    await page.close()
-    return data
 
 
 async def main() -> None:
     browser = BrowserManager()
-    await browser.start()
-    test_call_result = await scrape(browser, "Maria")
-    print(test_call_result)
+    await browser.start(headless=False)
+    captcha_manager = TelegramCaptchaManager()
+    test_call_result = await scrape(browser, captcha_manager, "Maria")
+    # print(test_call_result)
 
 
 if __name__ == "__main__":
