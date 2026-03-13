2d65402d939353c4501e5b1b9d34623837c73ce0
natan <natanael0junior@gmail.com>
2026-03-13 16:38:17 -0300

    feat: add support to fastapi documentation with swagger

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 from src.scraper.browser import BrowserManager
 from src.scraper.scraper import scrape
 from src.services.telegram_captcha import TelegramCaptchaManager
-from fastapi import FastAPI, Request
+from src.models.schemas import ScraperResponse, TelegramUpdate
+from fastapi import FastAPI, Request, Security, HTTPException, status
+from fastapi.security.api_key import APIKeyHeader
 from contextlib import asynccontextmanager
 from fastapi.responses import JSONResponse
+from dotenv import load_dotenv
 import asyncio
+import os
 
 
 @asynccontextmanager
@@ -24,10 +28,35 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+api_key_header = APIKeyHeader(name="X-API-KEY", auto_error=False)
+
+if not API_KEY:
+    raise ValueError("API Key não encontrada no env!")
 
 
-@app.get("/pessoa")
-async def pessoa(query: str):
+async def get_api_key(api_key_header: str = Security(api_key_header)):
+    if api_key_header == API_KEY:
+        return api_key_header
+    raise HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail="Acesso negado: Chave de API inválida",
+    )
+
+
+@app.get(
+    "/pessoa",
+    summary="Consulta beneficiário",
+    description="Realiza a busca no Portal da Transparência e resolve captchas via Telegram.",
+    response_model=ScraperResponse,
+)  # ScraperResponse é sua classe Pydantic)
+async def pessoa(query: str, api_key: str = Security(get_api_key)):
+    """
+    Endpoint utilizado para realizar a busca no Portal da Transparência
+    Recebe a query e a chave de api na url e no header, respectivamente.
+    """
+
     try:
         browser_manager = app.state.browser_manager
         captcha_manager = app.state.captcha_manager
@@ -36,17 +65,38 @@ async def pessoa(query: str):
         )
 
         return JSONResponse(content=result)
+    except ValueError as e:
+        return JSONResponse(
+            status_code=400,
+            content={"error": str(e)},
+        )
 
     except asyncio.TimeoutError:
         return JSONResponse(
             status_code=408,
             content={"error": "Tempo limite excedido."},
         )
+    except Exception as e:
+        print(e)
+        return JSONResponse(
+            status_code=500,
+            content={
+                "error": "Erro inesperado ao processar o scraping. Verifique os logs"
+            },
+        )
 
 
-@app.post("/telegram-webhook")
+@app.post(
+    "/telegram-webhook",
+    tags=["Webhooks"],
+    summary="Recebe resposta do Captcha",
+    response_model=TelegramUpdate,
+)
 async def telegram_webhook(request: Request):
-    # Rota passiva. Apenas recebe o POST do Telegram e avisa o Manager.
+    """
+    Endpoint utilizado pelo Telegram para notificar a resolução do captcha.
+    O CaptchaManager processa o 'update' e libera o Lock do scraper correspondente.
+    """
     update = await request.json()
 
     captcha_manager: TelegramCaptchaManager = app.state.captcha_manager
